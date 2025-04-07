d9e27cbfcfa32626f7fa07bde3826e50a7aecf17
saeed <mvvva001@gmail.com>
2025-04-07 22:03:10 +0500

    updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-cron');
 
 const token = '7406244528:AAG_22IBj01UW2_dqrQrQQKQD1LKDpMQxj0';
-const bot = new TelegramBot(token, { polling: false });
+const bot = new TelegramBot(token, { polling: true });
 const chatId = 5359819991;
 
 const lessonText = `
@@ -15,6 +15,6 @@ const lessonText = `
 `;
 
 cron.schedule('0 9 * * *', () => {
-  bot.sendMessage(chatId, lessonText);
-  console.log('Dars rejasi yuborildi!');
-});
+    bot.sendMessage(chatId, lessonText, { parse_mode: 'Markdown' });  // Markdown qo'shildi
+    console.log('Dars rejasi yuborildi!');
+  });

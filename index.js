1967a651dd96c05dd2920cf8ce73689a908bfc03
saeed <mvvva001@gmail.com>
2025-04-07 21:48:53 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+const cron = require('node-cron');
+
+const token = '7406244528:AAG_22IBj01UW2_dqrQrQQKQD1LKDpMQxj0';
+const bot = new TelegramBot(token, { polling: false });
+const chatId = 5359819991;
+
+const lessonText = `
+1-KUN | Ingliz tili intensiv
+- VOA Listening
+- Duolingo vocabulary
+- Speaking: Describe your morning
+- Writing: My daily routine
+`;
+
+cron.schedule('0 9 * * *', () => {
+  bot.sendMessage(chatId, lessonText);
+  console.log('Dars rejasi yuborildi!');
+});
